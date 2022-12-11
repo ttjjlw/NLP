3377dfbae27e200cb5f3ac8e8d68a95be3643b50
jialiangtu <jialiangtu@tencent.com>
2022-12-11 19:31:54 +0800

    u

---
+++ module/down_html/src/batfile/uploadB.bat
@@ -0,0 +1,7 @@
+cd D:\code\pycharm\project1\github\NLP\ && d:
+git pull origin main
+rem 222爆笑 223足球 224名人
+cd D:\code\pycharm\project1\github\NLP\module\down_html\src && d:
+python db.py --ip 127.0.0.1:9222 --video_addr "suiji" >> log/log9222.txt
+python db.py --ip 127.0.0.1:9223 --video_addr "suiji">> log/log9223.txt
+python db.py --ip 127.0.0.1:9224 --video_addr "minren">> log/log9224.txt
