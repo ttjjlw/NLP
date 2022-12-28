886f01b54fc5ec0042ab6f3fe05ecde9dc20b966
jialiangtu <jialiangtu@tencent.com>
2022-12-28 19:56:23 +0800

    u

--- module/down_html/src/batfile/dload_task.bat
+++ module/down_html/src/batfile/dload_task.bat
@@ -3,3 +3,4 @@ git pull origin main
 cd D:\code\pycharm\project1\github\NLP\module\down_html\src && d:
 python batchdy.py --isheadless 1 --istest 0 > log/logdload.txt
 
+git add module/down_html/src/downloaded_url/video_url.txt && git commit -m 'u' && git push origin main
