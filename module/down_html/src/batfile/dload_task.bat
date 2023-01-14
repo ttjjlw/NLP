84f2f49c2052e0dcdb256d7e817b343b0f230823
jialiangtu <jialiangtu@tencent.com>
2023-01-14 14:15:39 +0800

    m

--- module/down_html/src/batfile/dload_task.bat
+++ module/down_html/src/batfile/dload_task.bat
@@ -3,4 +3,5 @@ git pull origin main
 cd D:\code\pycharm\project1\github\NLP\module\down_html\src && d:
 python batchdy.py --isheadless 1 --istest 0 > log/logdload.txt
 
+cd D:\code\pycharm\project1\github\NLP\ && d:
 git add module/down_html/src/downloaded_url/video_url.txt && git commit -m 'dl' && git push origin main
--- module/down_html/src/batfile/get_core_data.bat
+++ module/down_html/src/batfile/get_core_data.bat
@@ -3,3 +3,4 @@ cd D:\code\pycharm\project1\github\NLP\module\down_html\src && d:
 rem taskkill /f /t /im chromedriver.exe
 rem taskkill /f /t /im chrome.exe
 python action_video.py --isheadless 1 --isplay 0 --issave 0 --istest 0 --isgetdata 1 > log/get_core_data.txt
+python send_email.py
