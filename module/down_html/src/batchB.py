fa91a549fc3dff938e860f15ccb1db58aa042b03
tjl <tjl@tencent.com>
2023-01-29 20:16:52 +0800

    deal with B video

---
+++ module/down_html/src/batchB.py
@@ -0,0 +1,37 @@
+﻿from moviepy.editor import *
+import re
+
+def download_video(tmp_dir,url='https://www.bilibili.com/video/BV1Z841137Rh/'):
+    p=os.popen('you-get --format=dash-flv480 -o %s %s'%(tmp_dir,url))
+    data=p.read()
+    title=re.findall('title:\s+(.+)',data)
+    print(title)
+    return title[0]
+def merge(tmp_dir,save_dir,title,url):
+    title1='%s%s[00].mp4'%(tmp_dir,title)
+    title2='%s%s[01].mp4'%(tmp_dir,title)
+    video = VideoFileClip(title1)
+    audio = AudioFileClip(title2)
+    video_merge = video.set_audio(audio)
+    video_merge.write_videofile("%s%s.mp4"%(save_dir,title),audio_codec='aac')
+    with open(f'{tmp_dir}dealed_url.txt','w') as f:
+        f.write(url+'\n')
+    os.remove(f'{title1}')
+    os.remove(f'{title2}')
+    os.remove(f'{tmp_dir}{title}.cmt.xml')
+
+if __name__ == '__main__':
+    tmp_dir = './tmp/'
+    if not os.path.exists(tmp_dir):
+        os.makedirs(tmp_dir)
+    save_dir = './tjl/'
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    try:
+        with open(f'{tmp_dir}dealed_url.txt','r') as f:
+            url_set=set(f.readlines())
+    except:
+        url_set={}
+
+    title=download_video(tmp_dir)
+    merge(tmp_dir,save_dir,title,url)
---
+++ module/down_html/src/video_clip.py
@@ -0,0 +1,20 @@
+﻿# 主要是需要moviepy这个库
+from moviepy.editor import *
+import os,random
+
+video = VideoFileClip("有意思的视频/1.mp4")
+
+# 剪辑视频，截取视频前0.1秒
+start=random.random()+random.randint(0,10)
+video_start = video.subclip(start,start+0.05)
+
+start=random.random()+random.randint(0,10)
+video_end = video.subclip(start,start+0.05)
+# 拼接视频
+final_clip = concatenate_videoclips([video_start,video,video_end])
+
+final_clip.to_videofile("有意思的视频/target.mp4", fps=24, remove_temp=False,audio_codec='aac')
+
+
+
+
